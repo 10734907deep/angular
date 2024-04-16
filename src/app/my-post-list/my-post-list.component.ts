778e28e2a83e98d7e2c2cb05c1c174d443ea57b9
10734907deep <166918025+10734907deep@users.noreply.github.com>
2024-04-16 17:12:48 +0530

    added view ,create comment and post blog
    added view ,create comment and post blog

---
+++ src/app/my-post-list/my-post-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-post-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-post-list.component.html',
+  styleUrl: './my-post-list.component.scss'
+})
+export class MyPostListComponent {
+
+}
